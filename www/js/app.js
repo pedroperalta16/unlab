faaea527890496ed3497047378299957fbabe83f
pochis <sheva852@hotmail.com>
2016-01-06 16:46:22 -0500

    v 0.0.1

--- www/js/app.js
+++ www/js/app.js
@@ -78,6 +78,65 @@
 
   }/*end login facebook*/
 
+  /*login con google*/
+  $scope.googleLogin  = function() {
+    document.addEventListener("deviceready", function () {
+
+         $cordovaOauth.google("300274685250-j35fcbdbo8hu8i3oh90dqovfub762uen.apps.googleusercontent.com", ["https://www.googleapis.com/auth/urlshortener", "https://www.googleapis.com/auth/userinfo.email"]).then(function(result) {
+            
+           //navigator.notification.activityStart('Espere por favor','Cargando');
+           $ionicLoading.show({
+              template: 'Cargando...'
+            });
+
+           $http.get("https://www.googleapis.com/plus/v1/people/me", {params: { access_token: result.access_token }}).then(function(result) {
+                 
+              var login ={
+                    'identifier' :result.data.id,
+                    'firstName': result.data.name.givenName,
+                    'photoURL' : result.data.image.url,
+                    'lastName' : result.data.name.familyName,
+                    'email': result.data.emails[0].value
+                  };
+              
+
+                $.post(endpoint+"api/mobile/socialLogin",{data:login},function(res){
+                        
+                        //navigator.notification.activityStop(); 
+                         $ionicLoading.hide();
+                       
+
+                            if(res.status=="ok"){
+
+                                   localStorage.setItem("id", res.data.id);
+                                   localStorage.setItem("name",  res.data.name);
+                                   localStorage.setItem("lastname",  res.data.lastname);
+                                   localStorage.setItem("correo",  res.data.correo);
+                                   localStorage.setItem("logged_in",  res.data.id);
+
+                                  
+                                  $location.path('/'+res.redirect);
+
+                            }else{
+                              alert("Ha ocurrido un error, vuelve a intentarlo mas tarde");
+                              
+                            }
+                  });
+                
+             
+ 
+              }, function(error) {
+                  alert("Ha ocurrido un error inesperado, vuelve a intentarlo mas tarde");
+              });
+
+          }, function(error) {
+             alert("Ha ocurrido un error inesperado, vuelve a intentarlo mas tarde");
+        });
+
+     },false);
+
+  }/*end login google*/
+
 
   });
 
