91b9df8d1e376f8aa115b8e3ca6e659224f20ae9
aarbelaez <aarbelaez@tribpub.com>
2016-05-20 15:48:28 -0300

    change the way of puswoosh trigger adding new files to get devices id 0.0.4

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,31 @@
 'use strict';
 (function(){
 
+  function initPushwoosh() {
+    var pushNotification = cordova.require("com.pushwoosh.plugins.pushwoosh.PushNotification");
+    if (device.platform == "Android") {
+      registerPushwooshAndroid();
+    }
+
+    if (device.platform == "iPhone" || device.platform == "iOS") {
+      registerPushwooshIOS();
+    }
+
+    if (device.platform == "Win32NT") {
+      registerPushwooshWP();
+    }
+
+    pushNotification.getLaunchNotification(
+      function(notification) {
+        if (notification != null) {
+          alert(JSON.stringify(notification));
+        } else {
+          alert("No launch notification");
+        }
+      }
+    );
+  }
+
   var app=angular.module('unlab', ['ionic','task','ngCordovaOauth','ngSanitize','ngCordova']);
   var resourceEndPoint="http://appunlab.comtic.co/"
 
