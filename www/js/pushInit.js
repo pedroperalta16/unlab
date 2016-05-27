57f5aac230a4532fb58f8012a760d7ec36fde21d
aarbelaez <aarbelaez@tribpub.com>
2016-05-27 12:53:04 -0300

    fixing deviceToken on ios

--- www/js/pushInit.js
+++ www/js/pushInit.js
@@ -25,27 +25,27 @@
 function initPushwoosh()
 {
     var pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
- 
+
     //set push notifications handler
     document.addEventListener('push-notification', function(event) {
         var title = event.notification.title;
         var userData = event.notification.userdata;
-                                 
+
         if(typeof(userData) != "undefined") {
             console.warn('user data: ' + JSON.stringify(userData));
-        }               
+        }
         alert(title);
     });
- 
+
     //initialize Pushwoosh with projectid: "GOOGLE_PROJECT_NUMBER", pw_appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
     pushNotification.onDeviceReady({ projectid: "300274685250", pw_appid : "10131-5FBF2" });
- 
+
     //register for pushes
     pushNotification.registerDevice(
         function(status) {
-            var pushToken = status;
+            var pushToken = (status['deviceToken']) ? status['deviceToken']: status;
             localStorage.setItem("device", pushToken);
-            console.warn('push token: ' + pushToken); 
+            console.warn('push token: ' + pushToken);
             //alert('push token: ' + pushToken)
         },
         function(status) {
